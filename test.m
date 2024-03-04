dbba87398b3fee3c30733e7ed8cb548909f53331
rogolop <45009376+rogolop@users.noreply.github.com>
2024-03-04 22:13:48 +0100

    test.m implement write to file

--- test.m
+++ test.m
@@ -1,57 +1,75 @@
-
 AttachSpec("SingularitiesDim2/IntegralClosureDim2.spec");
 Attach("MultiplierMeromorphic.m");
 
+
+printToFile        := false;
+outFileNamePrefix  := "examples/";
+outFileNameSufix   := ".txt";
+
+
+
+
+
 P<x, y> := LocalPolynomialRing(RationalField(), 2, "lglex");
 
+
+
 // Bernstein-Sato...
-// I := [P| y^3+x^5, x];
-// I := [P| y^3+x^5, y];
-// I := [P| y^3+x^5, 1];
+// s := "y^3+x^5, x";
+// s := "y^3+x^5, y";
+// s := "y^3+x^5, 1";
 
-// I := [P| x^5*y - y^3, x^8 + 2*x^5*y];
+// s := "x^5*y - y^3, x^8 + 2*x^5*y";
 
-// I := [P| x^2-y^3, y^2-x^3];
-// I := [P| x^3, y^2];
-// I := [P| y^2, x^3];
-// I := [P| x^3+y^2, y^2];
-// I := [P| x^3+x*y^2, y^2];
-// I := [P| (x^2-y^3)*(y^2-x^3), 1];
-// I := [P| (x^2-y^3)*(y^2-x^3), x^2*y^2];
+// s := "x^2-y^3, y^2-x^3";
+// s := "x^3, y^2";
+// s := "y^2, x^3";
+// s := "x^3+y^2, y^2";
+// s := "x^3+x*y^2, y^2";
+// s := "(x^2-y^3)*(y^2-x^3), 1";
+// s := "(x^2-y^3)*(y^2-x^3), x^2*y^2";
 
-// I := [P| (x^2-y^3)*(x^2+y^3), y^2-x^3];
-// I := [P| (x^2-y^3)^2, y^2-x^3];
-// I := [P| (x^2-y^3)*x, 1];
-// I := [P| (x^2-y^3)^2, 1];
-// I := [P| (x^2-y^3)*y, 1];
-// I := [P| y^2-x^3, (x^2-y^3)*(x^2+y^3)];
-// I := [P| y*(x^2-y^3)*(x^2+y^3), y^2-x^3];
-// I := [P| (x^9-y^4-x^3*y^3)^2 - x^16*y, y^2-x^3];
+// s := "(x^2-y^3)*(x^2+y^3), y^2-x^3";
+// s := "(x^2-y^3)^2, y^2-x^3";
+// s := "(x^2-y^3)*x, 1";
+// s := "(x^2-y^3)^2, 1";
+// s := "(x^2-y^3)*y, 1";
+// s := "y^2-x^3, (x^2-y^3)*(x^2+y^3)";
+// s := "y*(x^2-y^3)*(x^2+y^3), y^2-x^3";
+// s := "(x^9-y^4-x^3*y^3)^2 - x^16*y, y^2-x^3";
 
-I := [P| (x^2-y^3)*(x^2+y^3), 1];
-// I := [P| (x^2-y^3)^2, 1];
-// I := [P| (x^2-y^3), 1];
+s := "(x^2-y^3)*(x^2+y^3), 1";
+// s := "(x^2-y^3)^2, 1";
+// s := "(x^2-y^3), 1";
 
-// I := [P| (y^2-x^3)^5 + x^18, 1];
-// I := [P| (y^2-x^3)^5 + x^18, y^2-x^3];
-// I := [P| (y^2-x^3)^5 + x^18, (y^2-x^3)^2];
-// I := [P| (y^2-x^3)^5 + x^18, (y^2-x^3)^3];
-// I := [P| (y^2-x^3)^5 + x^18, (y^2-x^3)^4];
-// I := [P| (y^2-x^3)^5 + x^18, (y^2-x^3)^5];
+// s := "(y^2-x^3)^5 + x^18, 1";
+// s := "(y^2-x^3)^5 + x^18, y^2-x^3";
+// s := "(y^2-x^3)^5 + x^18, (y^2-x^3)^2";
+// s := "(y^2-x^3)^5 + x^18, (y^2-x^3)^3";
+// s := "(y^2-x^3)^5 + x^18, (y^2-x^3)^4";
+// s := "(y^2-x^3)^5 + x^18, (y^2-x^3)^5";
 
-// I := [P| (y^2-x^3)^5 + x^18, y^2+x^3];
+// s := "(y^2-x^3)^5 + x^18, y^2+x^3";
 
-// I := [P| (y^2-x^3)^4 + x^8*y^5, 1];
-// I := [P| (y^2-x^3)^4 + x^8*y^5, y^2-x^3];
-// I := [P| (y^2-x^3)^4 + x^8*y^5, y^2+x^3];
-
-
+// s := "(y^2-x^3)^4 + x^8*y^5, 1";
+// s := "(y^2-x^3)^4 + x^8*y^5, y^2-x^3";
+// s := "(y^2-x^3)^4 + x^8*y^5, y^2+x^3";
 
 
+f, g := eval s;
+f := P! f;
+g := P! g;
+// f, g := Explode(I);
 
-f, g := Explode(I);
-printf "f = %o\n", f;
-printf "g = %o\n", g;
+// Setup output
+sShort := &cat Split(s, "^* ");
+outFileName := outFileNamePrefix cat sShort cat outFileNameSufix;
+if printToFile then
+	SetOutputFile(outFileName : Overwrite := true);
+end if;
+
+printf "f = %o\n", Split(s, ", ")[1];
+printf "g = %o\n", Split(s, ", ")[2];
 // printf "\nIdeal I=<%o, %o>\n", Basis(I)[1], Basis(I)[2];
 
 // printf "\n### LogResolution\n";
